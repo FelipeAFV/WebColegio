e16ef53161bcc51ab0e9f01ef1cfc56fbafe07af
unknown <samuelborraccini@hotmail.com>
2020-11-11 19:40:54 -0300

    profesorTO

--- src/java/modelo/ProfesorDTO.java
+++ src/java/modelo/ProfesorDTO.java
@@ -10,5 +10,73 @@ package modelo;
  * @author samuel
  */
 public class ProfesorDTO {
+    private int id;
+    private String username;
+    private String passwoword;
+    private String name;
+    private String last_name;
+    private String email;
+    private String specialist;
+    
+    public ProfesorDTO(){
+        
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPasswoword() {
+        return passwoword;
+    }
+
+    public void setPasswoword(String passwoword) {
+        this.passwoword = passwoword;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSpecialist() {
+        return specialist;
+    }
+
+    public void setSpecialist(String specialist) {
+        this.specialist = specialist;
+    }
+    
     
 }
